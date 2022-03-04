b9663b8fcd6a39f5325c65e9da6a7abe367fe24d
Artyom Gutsulyak <38570098+Imweyboss@users.noreply.github.com>
2022-03-04 12:48:15 +0300

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,11 +1,9 @@
-@Library('test1') _
-
 pipeline{
     agent any
     stages{
         stage('Test'){
             steps{
-                welcome("Art")
+                echo 'Sucessfully'
             }
         }
     }
